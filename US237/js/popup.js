bfd2a3b22b932e3a84224e6ead0cd930b7d7ca6c
dpmcneil <155772121+dpmcneil@users.noreply.github.com>
2024-10-23 00:40:00 -0400

    US237 Task272
    This commit satisfies Task272 US237, optimizing the webpage for viewing
    on laptops and larger screens at 1080p and 1440p. This was committed
    under the wrong branch.

--- US237/js/popup.js
+++ US237/js/popup.js
@@ -222,28 +222,30 @@ function secondScreen() {
     
     // Update the content of the second screen
     popupContent.innerHTML = `
-      <div class="popup-content-header">
-        <h2>Quick Facts</h2>
-      </div>
-      <div class="popup-content-body">
-        <ul>
-          <li>Psyche was the 16<sup>th</sup> asteroid discovered, hence it is sometimes referred to as 16 Psyche.</li>
-          <li>M-Type Asteroid</li>
-          <li>Believed to be the core of a planetsimal</li>
-          <li>1 Year on Psyche is approximately 5 years on Earth</li>
-          <li>1 Day on Psyche is about 4 hours on Earth</li>
-          <li>Psyche has seasons</li>
-          <li>Psyche's temperature changes drastically between seasons (>100K / ~180&deg)</li>
-          <li>The Psyche mission is expected to reach the asteroid in 2029</li>
-        </ul>
-        <video id="video" controls autoplay muted loop>
-          <source src="images/psyche_popup_images/psyche_quick_overview.mp4" type="video/mp4">
-        </video>
-      </div>
-      <div class="popup-buttons">
-        <button id="prevButton">Previous</button>
-        <button id="closeButton">Close</button>
-        <button id="nextButton">Next</button>
+      <div id="screen2">
+        <div class="popup-content-header">
+          <h2>Quick Facts</h2>
+        </div>
+        <div class="popup-content-body">
+          <ul>
+            <li>Psyche was the 16<sup>th</sup> asteroid discovered, hence it is sometimes referred to as 16 Psyche.</li>
+            <li>M-Type Asteroid</li>
+            <li>Believed to be the core of a planetsimal</li>
+            <li>1 Year on Psyche is approximately 5 years on Earth</li>
+            <li>1 Day on Psyche is about 4 hours on Earth</li>
+            <li>Psyche has seasons</li>
+            <li>Psyche's temperature changes drastically between seasons (>100K / ~180&deg)</li>
+            <li>The Psyche mission is expected to reach the asteroid in 2029</li>
+          </ul>
+          <video id="video1" controls autoplay muted loop>
+            <source src="images/psyche_popup_images/psyche_quick_overview.mp4" type="video/mp4">
+          </video>
+        </div>
+        <div class="popup-buttons">
+          <button id="prevButton">Previous</button>
+          <button id="closeButton">Close</button>
+          <button id="nextButton">Next</button>
+        </div>
       </div>
     `;
     
@@ -280,21 +282,23 @@ function thirdScreen() {
     
     // Update content for the third screen
     popupContent.innerHTML = `
-      <div class="popup-content-header">
-        <h2>Size and Dimensions</h2>
-      </div>
-      <div class="popup-content-body">
-        <p>
-          16 Psyche is a potato-like shape, measuring 173 miles (280 kilometers) long at it widest spot, and 144 miles (232 kilometers) long. The total surface 
-          area is 64,000 square miles (165,800 square kilometers).<br><br> NASA Jet Propulsion Laboratory (JPL) has compared its size to the State of Maryland 
-          in the United States of America.
-        </p>
-        <img id="psyche1" src = "images/psyche_popup_images/Psyche_MD_3.jpg">
-      </div>
-      <div class="popup-buttons">
-        <button id="prevButton">Previous</button>
-        <button id="closeButton">Close</button>
-        <button id="nextButton">Next</button>
+      <div id="screen3">
+        <div class="popup-content-header">
+          <h2>Size and Dimensions</h2>
+        </div>
+        <div class="popup-content-body">
+          <p>
+            16 Psyche is a potato-like shape, measuring 173 miles (280 kilometers) long at it widest spot, and 144 miles (232 kilometers) long. The total surface 
+            area is 64,000 square miles (165,800 square kilometers).<br><br> NASA Jet Propulsion Laboratory (JPL) has compared its size to the State of Maryland 
+            in the United States of America.
+          </p>
+          <img id="psyche_size" src = "images/psyche_popup_images/Psyche_MD_3.jpg">
+        </div>
+        <div class="popup-buttons">
+          <button id="prevButton">Previous</button>
+          <button id="closeButton">Close</button>
+          <button id="nextButton">Next</button>
+        </div>
       </div>
     `;
     
@@ -331,26 +335,28 @@ function fourthScreen() {
     
     // Update content for the fourth screen
     popupContent.innerHTML = `
-      <div class="popup-content-header">
-        <h2>Life on Psyche</h2>
-      </div>
-      <div class="popup-content-body">
-        <p>
-          The scientific community believes that comets and asteroids are responsible for delivering key ingredients, including water, 
-          which is the key complex chemistry needed to sustain life on Earth. Some asteroids are believed to have the capability 
-          of supporting human life.
-        <br>
-        <br>
-          Currently, 16 Psyche does not show evidence of the critical nutrients of water, a breathable atmosphere, and a hospitable 
-          environment to allow for human life.
-        </p>
-        <img src="images/psyche_popup_images/ocean.jpg" id="ocean">
-      </div>
-        
-      <div class="popup-buttons">
-        <button id="prevButton">Previous</button>
-        <button id="closeButton">Close</button>
-        <button id="nextButton">Next</button>
+      <div id="screen4">
+        <div class="popup-content-header">
+          <h2>Life on Psyche</h2>
+        </div>
+        <div class="popup-content-body">
+          <p>
+            The scientific community believes that comets and asteroids are responsible for delivering key ingredients, including water, 
+            which is the key complex chemistry needed to sustain life on Earth. Some asteroids are believed to have the capability 
+            of supporting human life.
+          <br>
+          <br>
+            Currently, 16 Psyche does not show evidence of the critical nutrients of water, a breathable atmosphere, and a hospitable 
+            environment to allow for human life.
+          </p>
+          <img src="images/psyche_popup_images/ocean.jpg" id="ocean">
+        </div>
+          
+        <div class="popup-buttons">
+          <button id="prevButton">Previous</button>
+          <button id="closeButton">Close</button>
+          <button id="nextButton">Next</button>
+        </div>
       </div>
     `;
     
@@ -385,7 +391,7 @@ function fourthScreen() {
 function fifthScreen() {
     const popupContent = document.querySelector('.popup-content');
     popupContent.innerHTML = `
-      <div class="screen4">
+      <div id="screen5">
         <div class="popup-content-header">
           <h2>Surface of Psyche</h2>
         </div>
@@ -403,7 +409,7 @@ function fifthScreen() {
           <br>
             16 Psyche is believed to have survived multiple violent collisions during the solar system's formation.
           </p>
-          <img src="images/psyche_popup_images/Psyche_2.jpg" id="psyche2">
+          <img src="images/psyche_popup_images/Psyche_2.jpg" id="psyche_surface">
         </div>
         <div class="popup-buttons">
           <button id="prevButton">Previous</button>
@@ -446,26 +452,28 @@ function sixthScreen() {
     
     // Update content for the sixth screen
     popupContent.innerHTML = `
-      <div class="popup-content-header">  
-        <h2>Orbit and Rotation</h2>
-      </div>
-      <div class="popup-content-body">
-        <p>
-          16 Psyche is part of the Main Asteroid Belt, sitting between Mars and Jupiter. 
-          <br>
-          <br>
-          16 Psyche completes one rotation around the Sun, a Psyche year, approximately
-          every five Earth years. 
-          <br>
-          <br>
-          Psyche rotates along its axis, a Psyche day, about once every four hours.
-        </p>
-        <img src="images/psyche_popup_images/psyche_path.jpg" id="path">
-      </div>
-      <div class="popup-buttons">
-        <button id="prevButton">Previous</button>
-        <button id="closeButton">Close</button>
-        <button id="nextButton">Next</button>
+      <div id="screen6">
+        <div class="popup-content-header">  
+          <h2>Orbit and Rotation</h2>
+        </div>
+        <div class="popup-content-body">
+          <p>
+            16 Psyche is part of the Main Asteroid Belt, sitting between Mars and Jupiter. 
+            <br>
+            <br>
+            16 Psyche completes one rotation around the Sun, a Psyche year, approximately
+            every five Earth years. 
+            <br>
+            <br>
+            Psyche rotates along its axis, a Psyche day, about once every four hours.
+          </p>
+          <img src="images/psyche_popup_images/psyche_path.jpg" id="path_picture">
+        </div>
+        <div class="popup-buttons">
+          <button id="prevButton">Previous</button>
+          <button id="closeButton">Close</button>
+          <button id="nextButton">Next</button>
+        </div>
       </div>
     `;
     
@@ -502,7 +510,7 @@ function seventhScreen() {
     
     // Update content for the seventh screen
     popupContent.innerHTML = `
-    <div class="screen7">
+    <div id="screen7">
       <div class="popup-content-header">  
         <h2>Structure</h2>
       </div>
@@ -518,7 +526,7 @@ function seventhScreen() {
           ranging from 30-70% of the entire surface. The gravity on Psyche is much less than the Earth or 
           the Moon, where lifting a car on Psyche would be equivalent to lifting a dog on Earth.
         </p>
-        <video id="video" controls autoplay muted loop>
+        <video id="video2" controls autoplay muted loop>
           <source src="images/psyche_popup_images/psyche_video.mp4" type="video/mp4">
         </video>
       </div>
@@ -563,22 +571,24 @@ function eighthScreen() {
     
     // Update content for the eighth screen
     popupContent.innerHTML = `
-      <div class="popup-content-header">
-      <h2>Moons</h2>
-      </div>
-      <div class="popup-content-body">
-        <p>
-          NASA has identified more than 150 asteroids that have an orbiting moon, and in some cases more than one moon.
-          Other asteroids can have other asteroids orbiting each other, referred to as binary or triple asteroid systems.<br><br>16 Psyche
-          does not believe to have any moons or asteroids orbiting around it, however, more information will be known in 2029 when the Pscyche
-          mission is expected to reach the asteroid.
-        </p>
-        <img src="images/psyche_popup_images/moons.jpg" id="moons">
-      </div>
-      <div class="popup-buttons">
-        <button id="prevButton">Previous</button>
-        <button id="closeButton">Close</button>
-        <button id="nextButton">Next</button>
+      <div id="screen8">
+        <div class="popup-content-header">
+        <h2>Moons</h2>
+        </div>
+        <div class="popup-content-body">
+          <p>
+            NASA has identified more than 150 asteroids that have an orbiting moon, and in some cases more than one moon.
+            Other asteroids can have other asteroids orbiting each other, referred to as binary or triple asteroid systems.<br><br>16 Psyche
+            does not believe to have any moons or asteroids orbiting around it, however, more information will be known in 2029 when the Pscyche
+            mission is expected to reach the asteroid.
+          </p>
+          <img src="images/psyche_popup_images/moons.jpg" id="moon_picture">
+        </div>
+        <div class="popup-buttons">
+          <button id="prevButton">Previous</button>
+          <button id="closeButton">Close</button>
+          <button id="nextButton">Next</button>
+        </div>
       </div>
     `;
     
@@ -615,7 +625,7 @@ function ninthScreen() {
     
     // Update content for the ninth screen
     popupContent.innerHTML = `
-    <div class="rings">
+    <div id="screen9">
       <div class="popup-content-header">
         <h2>Rings</h2>
       </div>
@@ -624,7 +634,7 @@ function ninthScreen() {
           While a few asteroids are known to have rings, 16 Psyche does not possess any.
         </p>
         <div>
-        <img src="images/psyche_popup_images/rings.jpg" id="rings">
+        <img src="images/psyche_popup_images/rings.jpg" id="ring_picture">
         </div>
       </div>
       <div class="popup-buttons">
@@ -667,21 +677,23 @@ function tenthScreen() {
     
     // Update content for the tenth screen
     popupContent.innerHTML = `
-      <div class="popup-content-header">
-        <h2>Atmosphere</h2>
-      </div>
-      <div class="popup-content-body">
-        <p>
-          Like all asteroids, 16 Psyche does not have an atmosphere. Atmospheric layers are crucial for protecting planets by shielding them from impacts. 
-        <br>
-        <br>
-          Without an atmosphere, 16 Psyche is exposed to collisions with asteroids, comets, and other space debris.
-        </p>
-        <img src="images/psyche_popup_images/aurora.jpg" id="atmosphere">
+      <div id="screen10">
+        <div class="popup-content-header">
+          <h2>Atmosphere</h2>
         </div>
-      <div class="popup-buttons">
-        <button id="prevButton">Previous</button>
-        <button id="closeButton">Close</button>
+        <div class="popup-content-body">
+          <p>
+            Like all asteroids, 16 Psyche does not have an atmosphere. Atmospheric layers are crucial for protecting planets by shielding them from impacts. 
+          <br>
+          <br>
+            Without an atmosphere, 16 Psyche is exposed to collisions with asteroids, comets, and other space debris.
+          </p>
+          <img src="images/psyche_popup_images/aurora.jpg" id="atmosphere">
+          </div>
+        <div class="popup-buttons">
+          <button id="prevButton">Previous</button>
+          <button id="closeButton">Close</button>
+        </div>
       </div>
     `;
     
