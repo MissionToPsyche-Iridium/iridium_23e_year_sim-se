ff0cf4931532db96d922a161ef7e117b822957bd
Jenny Potocki <jenny.potocki@gmail.com>
2024-11-06 12:30:38 -0800

    Progress for Task340

--- Psyche/js/reference_section.js
+++ Psyche/js/reference_section.js
@@ -1,19 +1,20 @@
 const toggleButton = document.getElementById('toggleReferences');
 const references = document.getElementById('references');
 
-function toggleReferences() {
+/**function toggleReferences() {
     const referencesList = document.getElementById('referencesList');
     referencesList.classList.toggle('hide');
   }
-
+*/
   document.getElementById('toggleReferences').addEventListener('click', function() {
     const referencesList = document.getElementById('referencesList');
-    referencesList.classList.toggle("hide"); // Toggle visibility
+    referencesList.classList.toggle('hide'); // Toggle visibility
 });
-  toggleButton.addEventListener('click', function() {
+ /**  toggleButton.addEventListener('click', function() {
     if (references.style.display == 'none' || references.style.display == '') {
         references.style.display = 'block'; 
     } else {
         references.style.display = 'none';
     }
     });
+*/
