f9a56e649636937dac809ba966a318b3daa1ff1a
dpmcneil <dpmcneil@asu.edu>
2025-03-03 16:14:45 -0500

    US619 Task628
    This commit enhances the surface features section of the surface page, adding a new background animation, and animations within the div. This will enhance the user experience for the end user.

--- JS/surface2.js
+++ JS/surface2.js
@@ -2,7 +2,7 @@ gsap.set([
     "#page-header", 
     "#header-h1",
     "#materials", 
-    "#div2", 
+    "#features", 
     "#div3", 
     "#div4", 
     "#div5",
@@ -20,8 +20,8 @@ const tl = gsap.timeline({
 
 // Animate background size without repeating
 tl.fromTo("body", 
-    { backgroundSize: "300%" },  // Start large and zoomed in
-    { backgroundSize: "700%", duration: 3 } // Shrink to fit screen properly
+    { backgroundSize: "300%"},  // Start large and zoomed in
+    { backgroundSize: "650%", duration: 3 } // Shrink to fit screen properly
 );
 
 tl.fromTo(
@@ -43,21 +43,34 @@ tl.to("#header-h1", {opacity: 1, duration: 3})
 
 // Smooth transition from background image to black, then to white
 tl.to("body", { backgroundImage: "none", backgroundColor: "black", duration: 1}, "<60%");
-tl.to("body", { backgroundColor: "white", backgroundSize: "100%", duration: 4});
+tl.to("body", { backgroundColor: "white", backgroundSize: "100%", duration: 1});
 
 tl.to("#materials", {opacity: 1}, "<50%");
 tl.fromTo(
+    "body", 
+    { 
+        backgroundColor: "white" 
+    }, 
+    { 
+        backgroundColor: "black", 
+        duration: 2, 
+        ease: "power4.inOut", 
+        scrollTrigger: {
+            trigger: '#materials',
+            start: 'top center',
+            end: '20% center',
+            scrub: true
+        }
+  })
+
+tl.fromTo(
     "#materials-h1, #materials-p", 
     {
-        opacity: 0,
-        x: -3000,
-        rotate: 30
+        opacity: 0
     },
     {
         opacity: 1,
-        x: 0,
-        rotate: 0,
-        duration: 2,
+        duration: 3,
         ease: "power4.in"
     }, "<"
 );
@@ -128,7 +141,7 @@ tl.fromTo(
         backgroundSize: "100%"
     },
     {
-        backgroundSize: "300%",
+        backgroundSize: "400%",
         duration: 7,
         ease: "power4.in",  
         scrollTrigger: {
@@ -140,7 +153,172 @@ tl.fromTo(
     }
 )
 
+tl.to("#features", {opacity: 1}, "<")
 
-tl.to(["#div2", "#div3", "#div4", "#div5", "#footer"], {opacity: 1, duration: 4});
+tl.fromTo(
+    "#features", 
+    {
+        backgroundSize: "300%",
+        backgroundPosition: "40% 0%"
+    },
+    {
+        backgroundSize: "200%",
+        backgroundPosition: "50% 50%",
+        duration: 4,
+        ease: "power4.in",  
+        scrollTrigger: {
+            trigger: '#features',
+            start: 'top center',
+            end: '50% center',
+            scrub: true
+        } 
+    }
+)
+
+tl.fromTo(
+    "#inner-features",
+    {
+        color: "#ef5966",
+        backgroundColor: "rgba(0, 0, 0, 1)"
+    },
+    {
+        color: "white",
+        backgroundColor: "rgba(0, 0, 0, 0.6)",
+        scrollTrigger: {
+            trigger: '#features',
+            start: '30% center',
+            end: 'bottom center',
+            scrub: true
+        } 
+    }, "<"
+)
+
+tl.fromTo(
+    "#features-h1", 
+    {
+        scale: 1,
+        y: 0
+    },
+    {
+        scale: 3,
+        y: -200,
+        duration: 3,
+        ease: "power4.in",  
+        scrollTrigger: {
+            trigger: '#features',
+            start: 'top center',
+            end: '50% center',
+            scrub: true
+        } 
+    }, "<"
+)
+
+
+
+tl.fromTo(
+    "#features-p",
+    {
+        y: 0
+    },
+    {
+        y: 250,
+        paddingBottom: "5%",
+        scrollTrigger: {
+            trigger: '#features',
+            start: '50% center',
+            end: '90% center',
+            scrub: true
+        } 
+    }, "<"
+)
+
+tl.fromTo(
+    "#features-h1",
+    {
+        scale: 3,
+        y: -200
+    },
+    {
+        scale: 2,
+        y: 200,
+        duration: 3,
+        ease: "power4.in", 
+        scrollTrigger: {
+            trigger: '#features',
+            start: '50% center',
+            end: '80% center',
+            scrub: true
+        } 
+    }, "<"
+)
+
+tl.to(
+    "#features, #inner-features",
+    {
+        borderRadius: "10rem",
+        duration: 4,
+        ease: "power4.inOut",
+        scrollTrigger: {
+            trigger: '#features',
+            start: '70% center',
+            end: 'bottom center',
+            scrub: true
+        } 
+    }
+)
+
+tl.fromTo(
+    "#features",
+    {
+        backgroundSize: "200%",
+        backgroundPosition: "50% 50%",
+    },
+    {
+        backgroundSize: "300%",
+        backgroundPosition: "-10% 0%",
+        duration: 4,
+        ease: "power4.inOut",
+        scrollTrigger: {
+            trigger: '#features',
+            start: '70% center',
+            end: 'bottom center',
+            scrub: true
+        } 
+    }
+)
+
+tl.to(
+    "#features",
+    {
+        backgroundColor: "rgba(0, 0, 0, 1)",
+        backgroundImage: "none",
+        duration: 4,
+        ease: "power4.inOut",
+        scrollTrigger: {
+            trigger: '#features',
+            start: '90% center',
+            end: 'bottom center',
+            scrub: true
+        } 
+    }
+)
+
+tl.to(
+    "#inner-features",
+    {
+        color: "#ef5966",
+        scale: 1,
+        duration: 4,
+        scrollTrigger: {
+            trigger: '#features',
+            start: '90% center',
+            end: 'bottom center',
+            scrub: true
+        } 
+    }
+)
+
+
+tl.to(["#div3", "#div4", "#div5", "#footer"], {opacity: 1, duration: 4});
 
 
