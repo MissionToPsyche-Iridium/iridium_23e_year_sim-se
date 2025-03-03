ec1fc2ebf8c4e5a64f590b4a343cc79e8d0245a9
dpmcneil <dpmcneil@asu.edu>
2025-03-03 14:21:27 -0500

    US619 Task627
    This commit enhances the look of the material section, adding a new picture that moves with the mouse, along with color changing and moving text. This adds an enhanced experience to the end user.

--- JS/surface2.js
+++ JS/surface2.js
@@ -1,7 +1,7 @@
 gsap.set([
     "#page-header", 
     "#header-h1",
-    "#div1", 
+    "#materials", 
     "#div2", 
     "#div3", 
     "#div4", 
@@ -44,4 +44,103 @@ tl.to("#header-h1", {opacity: 1, duration: 3})
 // Smooth transition from background image to black, then to white
 tl.to("body", { backgroundImage: "none", backgroundColor: "black", duration: 1}, "<60%");
 tl.to("body", { backgroundColor: "white", backgroundSize: "100%", duration: 4});
-tl.to(["#div1", "#div2", "#div3", "#div4", "#div5", "#footer"], {opacity: 1, duration: 4});
+
+tl.to("#materials", {opacity: 1}, "<50%");
+tl.fromTo(
+    "#materials-h1, #materials-p", 
+    {
+        opacity: 0,
+        x: -3000,
+        rotate: 30
+    },
+    {
+        opacity: 1,
+        x: 0,
+        rotate: 0,
+        duration: 2,
+        ease: "power4.in"
+    }, "<"
+);
+
+tl.fromTo(
+    "#materials-h1", 
+    {
+        scale: 2,
+        y: 0
+    },
+    {
+        scale: 3,
+        y: -200,
+        duration: 3,
+        ease: "power4.in",  
+        scrollTrigger: {
+            trigger: '#materials',
+            start: 'top center',
+            end: '50% center',
+            scrub: true
+        } 
+    }
+)
+
+tl.fromTo(
+    "#materials-h1", 
+    {
+        scale: 3,
+        y: -200
+    },
+    {
+        scale: 2,
+        y: 200,
+        duration: 3,
+        ease: "power4.in",  
+        color: "#ef5966", 
+        scrollTrigger: {
+            trigger: '#materials',
+            start: '50% center',
+            end: '90% center',
+            scrub: true
+        } 
+    }
+)
+
+tl.fromTo(
+    "#materials-p", 
+    {
+        y: 0
+    },
+    {
+        y: 200,
+        duration: 3,
+        ease: "power4.in",
+        color: "#ef5966",  
+        scrollTrigger: {
+            trigger: '#materials',
+            start: 'top center',
+            end: '85% center',
+            scrub: true
+        } 
+    }
+)
+
+tl.fromTo(
+    "#materials", 
+    {
+        backgroundSize: "100%"
+    },
+    {
+        backgroundSize: "300%",
+        duration: 7,
+        ease: "power4.in",  
+        scrollTrigger: {
+            trigger: '#materials',
+            start: '10% center',
+            end: 'bottom center',
+            scrub: true
+        } 
+    }
+)
+
+
+tl.to(["#div2", "#div3", "#div4", "#div5", "#footer"], {opacity: 1, duration: 4});
+
+
