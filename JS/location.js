01be1df7c912e1621a39e82717bb0987f3b55ff7
dpmcneil <dpmcneil@asu.edu>
2025-03-22 12:42:12 -0400

    US669 Task676
    This commit fixes responsiveness in smaller laptop screens.

--- JS/location.js
+++ JS/location.js
@@ -83,83 +83,89 @@ tl.fromTo(
     }, "<"
 )
 
-
-
 tl.to('body', {background: 'white', delay: 0, opacity: 1})
 
-tl.fromTo(
-    '#div2',
-    { 
-        x: 1000, 
-        opacity: 0 
-    },
-    {
-        x: 0,
-        opacity: 1,
-        duration: 7,
-        scrollTrigger: {
-            trigger: '#div2',
-            start: 'top center',
-            end: '40% center',
-            scrub: true
+if (window.innerWidth > "1100px") {
+    tl.fromTo(
+        '#div2',
+        { 
+            x: 1000, 
+            opacity: 0 
+        },
+        {
+            x: 0,
+            opacity: 1,
+            duration: 7,
+            scrollTrigger: {
+                trigger: '#div2',
+                start: 'top center',
+                end: '40% center',
+                scrub: true
+            }
         }
-    }
-);
+    );
 
-tl.fromTo(
-    '#div3',
-    {
-        opacity: 0
-    },
-    {
-        opacity: 1,
-        duration: 7,
-        scrollTrigger: {
-            trigger: '#div3',
-            start: 'top center',
-            end: '60% center',
-            scrub: true
+    tl.fromTo(
+        '#div3',
+        {
+            opacity: 0
+        },
+        {
+            opacity: 1,
+            duration: 7,
+            scrollTrigger: {
+                trigger: '#div3',
+                start: 'top center',
+                end: '60% center',
+                scrub: true
+            }
         }
-    }
-);
+    );
 
-tl.fromTo(
-    '#div4',
-    {
-        opacity: 0,
-        x: -1000,
-        pointerEvents: "none"
-    },
-    {
-        opacity: 1,
-        x: 0,
-        duration: 2,
-        pointerEvents: "auto",
-        scrollTrigger: {
-            trigger: '#div4',
-            start: '-50% center',
-            end: '50% center',
-            scrub: true
+    tl.fromTo(
+        '#div4',
+        {
+            opacity: 0,
+            x: -1000,
+            pointerEvents: "none"
+        },
+        {
+            opacity: 1,
+            x: 0,
+            duration: 2,
+            pointerEvents: "auto",
+            scrollTrigger: {
+                trigger: '#div4',
+                start: '-50% center',
+                end: '50% center',
+                scrub: true
+            }
         }
-    }
-);
+    );
 
-tl.fromTo(
-    'footer',
-    {
-        opacity: 0,
-        y: 100
-    },
-    {
-        opacity: 1,
-        y: 0,
-        duration: 7,
-        scrollTrigger: {
-            trigger: '#div4',
-            start: '40% center',
-            end: '60% center',
-            scrub: true
+    tl.fromTo(
+        'footer',
+        {
+            opacity: 0,
+            y: 100
+        },
+        {
+            opacity: 1,
+            y: 0,
+            duration: 7,
+            scrollTrigger: {
+                trigger: '#div4',
+                start: '40% center',
+                end: '60% center',
+                scrub: true
+            }
         }
-    }
-);
+    );
+} else {
+    gsap.set([
+        "#div2", 
+        "#div3", 
+        "#div4", 
+    ], { opacity: 1 });
+}
 
